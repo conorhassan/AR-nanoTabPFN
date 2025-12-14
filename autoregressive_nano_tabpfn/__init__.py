3946c9c549fa78bb58de88a2f29ead1121f5212c
conorhassan <conordhassan@gmail.com>
2025-12-14 17:32:57 +0200

    add autoregressive-nanoTabPFN module

---
+++ autoregressive_nano_tabpfn/__init__.py
@@ -0,0 +1,45 @@
+"""autoregressive-nanoTabPFN: Autoregressive TabPFN with two-stage attention."""
+
+from .model import (
+    ARTabPFN,
+    Embedder,
+    TwoStageTransformer,
+    TwoStageTransformerLayer,
+    MixtureGaussianHead,
+    MultiheadAttention,
+    create_dense_mask,
+    create_row_mask,
+    create_context_self_attention_mask,
+    clear_mask_cache,
+    triton_available,
+    cross_attention,
+)
+from .data import (
+    DataAttr,
+    MLPSCM,
+    TabularSampler,
+    OnlineTabularDataset,
+)
+
+__all__ = [
+    # Model
+    'ARTabPFN',
+    'Embedder',
+    'TwoStageTransformer',
+    'TwoStageTransformerLayer',
+    'MixtureGaussianHead',
+    # Attention
+    'MultiheadAttention',
+    'create_dense_mask',
+    'create_row_mask',
+    'create_context_self_attention_mask',
+    'clear_mask_cache',
+    # Triton
+    'triton_available',
+    'cross_attention',
+    # Data
+    'DataAttr',
+    'MLPSCM',
+    'TabularSampler',
+    'OnlineTabularDataset',
+]
